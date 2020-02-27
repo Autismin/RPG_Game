cdd4edeef3fc593e86c678d4f91e1e4bce5ff721
Autismin <martin.gg222@gmail.com>
2020-02-27 01:29:23 +0100

    1.0.2

--- src/Paquete/Ventana.java
+++ src/Paquete/Ventana.java
@@ -8,6 +8,8 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Random;
+
 import javax.swing.SwingConstants;
 
 public class Ventana extends JFrame implements MouseListener {
@@ -20,18 +22,20 @@ public class Ventana extends JFrame implements MouseListener {
 	
 	int monedas = 0;
 	int mejora = 1;
+	int costeMejora = 25;
 	
 	JPanel panel = new JPanel();
-	JLabel etiqueta1 = new JLabel();
-	JLabel etiqueta2 = new JLabel();
-	JLabel etiqueta3 = new JLabel();
+	JLabel sumarPuntos = new JLabel();
+	JLabel dinero = new JLabel();
+	JLabel mejoraDinero = new JLabel();
+	JLabel costeUpgrade = new JLabel();
 	
 	public Ventana () {
 		
 		setSize(ANCHO_ESCALADO,ALTO_ESCALADO);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		setTitle("RPG Game 1.0.1");
+		setTitle("RPG Game 1.0.2");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		Panel1();
 	}
@@ -42,58 +46,87 @@ public class Ventana extends JFrame implements MouseListener {
 		panel.setBackground(Color.black);
 		this.getContentPane().add(panel);
 		
-		etiqueta1.setSize(ANCHO_ESCALADO/9,ALTO_ESCALADO/12);
-		etiqueta1.setLocation(ANCHO_ESCALADO/2-ANCHO_ESCALADO/18,ALTO_ESCALADO-ALTO_ESCALADO/6);
-		etiqueta1.setOpaque(true);
-		etiqueta1.setBackground(Color.white);
-		etiqueta1.setText("+1");
-		etiqueta1.setFont(new Font("unispace",0,ANCHO_ESCALADO/80));
-		etiqueta1.setForeground(Color.black);
-		etiqueta1.setHorizontalAlignment(SwingConstants.CENTER);
-		etiqueta1.addMouseListener(this);
-		panel.add(etiqueta1);
+		sumarPuntos.setSize(ANCHO_ESCALADO/9,ALTO_ESCALADO/12);
+		sumarPuntos.setLocation(ANCHO_ESCALADO/2-ANCHO_ESCALADO/18,ALTO_ESCALADO-ALTO_ESCALADO/6);
+		sumarPuntos.setOpaque(true);
+		sumarPuntos.setBackground(Color.white);
+		sumarPuntos.setText("+"+mejora);
+		sumarPuntos.setFont(new Font("unispace",0,ANCHO_ESCALADO/80));
+		sumarPuntos.setForeground(Color.black);
+		sumarPuntos.setHorizontalAlignment(SwingConstants.CENTER);
+		sumarPuntos.addMouseListener(this);
+		panel.add(sumarPuntos);
 		
-		etiqueta2.setSize(ANCHO_ESCALADO/12,ALTO_ESCALADO/16);
-		etiqueta2.setLocation(ANCHO_ESCALADO*8/9,ALTO_ESCALADO/25);
-		etiqueta2.setOpaque(true);
-		etiqueta2.setBackground(Color.white);
-		etiqueta2.setText(String.valueOf(monedas));
-		etiqueta2.setFont(new Font("unispace",0,ANCHO_ESCALADO/80));
-		etiqueta2.setForeground(Color.black);
-		etiqueta2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(etiqueta2);
+		dinero.setSize(ANCHO_ESCALADO/12,ALTO_ESCALADO/16);
+		dinero.setLocation(ANCHO_ESCALADO*8/9,ALTO_ESCALADO/25);
+		dinero.setOpaque(true);
+		dinero.setBackground(Color.white);
+		dinero.setText(String.valueOf(monedas));
+		dinero.setFont(new Font("unispace",0,ANCHO_ESCALADO/80));
+		dinero.setForeground(Color.black);
+		dinero.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(dinero);
 		
-		etiqueta3.setSize(ANCHO_ESCALADO/12,ALTO_ESCALADO/16);
-		etiqueta3.setLocation(ANCHO_ESCALADO/40,ALTO_ESCALADO/25);
-		etiqueta3.setOpaque(true);
-		etiqueta3.setBackground(Color.white);
-		etiqueta3.setText("+1/click");
-		etiqueta3.setFont(new Font("unispace",0,ANCHO_ESCALADO/80));
-		etiqueta3.setForeground(Color.black);
-		etiqueta3.setHorizontalAlignment(SwingConstants.CENTER);
-		etiqueta3.addMouseListener(this);
-		panel.add(etiqueta3);
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {}
-	
-	@Override
-	public void mouseEntered(MouseEvent arg0) {}
-	
-	@Override
-	public void mouseExited(MouseEvent arg0) {}
-	
-	@Override
-	public void mousePressed(MouseEvent arg0) {
+		mejoraDinero.setSize(ANCHO_ESCALADO/12,ALTO_ESCALADO/16);
+		mejoraDinero.setLocation(ANCHO_ESCALADO/40,ALTO_ESCALADO/25);
+		mejoraDinero.setOpaque(true);
+		mejoraDinero.setBackground(Color.white);
+		mejoraDinero.setText("+1/click");
+		mejoraDinero.setFont(new Font("unispace",0,ANCHO_ESCALADO/80));
+		mejoraDinero.setForeground(Color.black);
+		mejoraDinero.setHorizontalAlignment(SwingConstants.CENTER);
+		mejoraDinero.addMouseListener(this);
+		panel.add(mejoraDinero);
 		
-		panel.setBackground(Color.red);
-		monedas = monedas + mejora;
-		etiqueta2.setText(String.valueOf(monedas));
+		costeUpgrade.setSize(ANCHO_ESCALADO/12,ALTO_ESCALADO/16);
+		costeUpgrade.setLocation(ANCHO_ESCALADO/40,ALTO_ESCALADO/9);
+		costeUpgrade.setOpaque(true);
+		costeUpgrade.setBackground(Color.white);
+		costeUpgrade.setText(String.valueOf(costeMejora));
+		costeUpgrade.setFont(new Font("unispace",0,ANCHO_ESCALADO/80));
+		costeUpgrade.setForeground(Color.black);
+		costeUpgrade.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(costeUpgrade);
+		
 	}
 	
+	public void actDinero(int mod) {
+		
+		monedas = monedas + mod;
+		dinero.setText(String.valueOf(monedas));
+	}
+	
 	@Override
-	public void mouseReleased(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent e) {}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	
+	@Override
+	public void mouseExited(MouseEvent e) {}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		if (e.getComponent().equals(sumarPuntos)) {
+			
+			panel.setBackground(Color.red);
+	        actDinero(mejora);
+		}
+	
+		if (e.getComponent().equals(mejoraDinero) && monedas>=costeMejora) {
+			
+			actDinero(-costeMejora);
+			mejora++;
+			sumarPuntos.setText("+"+mejora);
+			Random r = new Random();
+			costeMejora = costeMejora+r.nextInt(costeMejora);
+			costeUpgrade.setText(String.valueOf(costeMejora));
+		}
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
 		
 		panel.setBackground(Color.black);
 	}
